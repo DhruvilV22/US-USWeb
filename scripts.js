0ac17f7f9ad886382aea4cc5fcc726a9f2362698
DhruvilV22 <dhruvil.vyas05@gmail.com>
2025-01-02 01:13:00 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
-// JavaScript for Hamburger Toggle
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle the visibility of the nav links
+    navLinks.classList.toggle('active');  // Toggle the visibility of the nav links
     hamburger.classList.toggle('active'); // Toggle the hamburger icon animation
 });
 
-        window.addEventListener('scroll', function () {
-            const hero = document.querySelector('.hero');
-            const floatingButton = document.querySelector('.floating-button');
+window.addEventListener('scroll', function () {
+    const hero = document.querySelector('.hero');
+    const floatingButton = document.querySelector('.floating-button');
 
-            const heroBottom = hero.getBoundingClientRect().bottom;
+    const heroBottom = hero.getBoundingClientRect().bottom;
 
-            if (heroBottom < 0) {
-                floatingButton.classList.add('show');
-            } else {
-                floatingButton.classList.remove('show');
-            }
-        });
+    if (heroBottom < 0) {
+        floatingButton.classList.add('show');
+    } else {
+        floatingButton.classList.remove('show');
+    }
+});
