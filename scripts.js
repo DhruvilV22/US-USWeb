c16dd682dda45c998a779848666328d94a0a5c3e
DhruvilV22 <dhruvil.vyas05@gmail.com>
2025-01-02 01:02:31 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,3 +6,16 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // Toggle the visibility of the nav links
     hamburger.classList.toggle('active'); // Toggle the hamburger icon animation
 });
+
+        window.addEventListener('scroll', function () {
+            const hero = document.querySelector('.hero');
+            const floatingButton = document.querySelector('.floating-button');
+
+            const heroBottom = hero.getBoundingClientRect().bottom;
+
+            if (heroBottom < 0) {
+                floatingButton.classList.add('show');
+            } else {
+                floatingButton.classList.remove('show');
+            }
+        });
